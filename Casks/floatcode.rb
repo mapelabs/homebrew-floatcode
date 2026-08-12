b77aef2a6af83a6cf9b86d9c9476a53cd32e33c6
Luís Pereira <luis@devlandia.net>
2026-08-12 10:42:40 -0300

    chore: update floatcode to 1.0.2-alpha

--- Casks/floatcode.rb
+++ Casks/floatcode.rb
@@ -1,6 +1,6 @@
 cask "floatcode" do
-  version "1.0.0-alpha"
-  sha256 "b971d58af40d914bb1472942d6a72e01534cfad8206e9e8cf9d0ba0654f60803"
+  version "1.0.2-alpha"
+  sha256 "0a900ff61fb3591fc75177868cd80ebaffd20ddf25a1ad9c7839ae420159b88a"
 
   url "https://storage.googleapis.com/floatcode.firebasestorage.app/releases/FloatCode.dmg"
   name "FloatCode"
